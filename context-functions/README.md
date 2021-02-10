206cd8f45a958dd8f6db3aa2e19fc7831b08d079
Maciej Modzelewski <maciej@modzelewski.dev>
2021-02-10 15:18:44 +0100

    Add context functions example

---
+++ context-functions/README.md
@@ -0,0 +1,11 @@
+## Scala 3 Context Functions
+
+The code was prepared based on [this video](https://www.youtube.com/watch?v=YXDm3WHZT5g) and updated to the current
+syntax of Scala version 3.0.0-M3.
+
+[Context functions documentation](https://dotty.epfl.ch/docs/reference/contextual/context-functions.html)
+
+### Usage
+
+- compile: `sbt compile`
+- run: `sbt run`
---
+++ context-functions/src/main/scala/Main.scala
@@ -0,0 +1,68 @@
+case class Config(name: String, age: Int)
+
+case class Name(firstName: String, lastName: String)
+
+case class Age(age: Int)
+
+case class Person(name: Name, age: Age)
+
+object Imperative {
+
+  import Configs._
+  import Exceptions._
+
+  def readName: Possibly[Configured[Name]] = {
+    val parts = config.name.split(" ")
+    require(parts.length >= 2)
+    Name(parts(0), parts.tail.mkString(" "))
+  }
+
+  def readAge: Possibly[Configured[Age]] = {
+    val age = config.age
+    require(1 <= age && age <= 150)
+    Age(age)
+  }
+
+  def readPerson: Configured[Option[Person]] = {
+    attempt(
+      Some(Person(readName, readAge))
+    ).onError(None)
+  }
+
+  @main def hello: Unit = {
+    println(readPerson(using Config("John Doe", 20)))
+    println(readPerson(using Config("Incognito", 99)))
+  }
+
+}
+
+object Configs {
+  type Configured[T] = Config ?=> T
+
+  def config: Configured[Config] = implicitly[Config]
+}
+
+object Exceptions {
+
+  private class E extends Exception
+
+  class CanThrow private[Exceptions]() {
+    private[Exceptions] def throwE() = throw new E
+  }
+
+  type Possibly[T] = CanThrow ?=> T
+
+  def require(p: Boolean)(using ct: CanThrow): Unit =
+    if (!p) ct.throwE()
+
+  def attempt[T](op: Possibly[T]) = OnError(op)
+
+  class OnError[T](op: Possibly[T]) {
+    def onError(fallback: => T): T =
+      try op(using new CanThrow)
+      catch {
+        case ex: E => fallback
+      }
+  }
+
+}
